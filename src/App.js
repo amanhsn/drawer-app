d98fb4950427736b7e0cf80feb688a7ac7a4dd23
amanhsn <zilleh67@gmail.com>
2023-05-08 11:35:02 +0500

    Added Styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,184 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import statusData from './statusData.json';
+import { withStyles } from '@material-ui/core/styles';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  TextField,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { useTableSearch } from 'react-table';
 import './App.css';
+const StyledExpandMoreIcon = withStyles({
+  root: {
+    color: 'white',
+  },
+})(ExpandMoreIcon);
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  accordion: {
+    marginBottom: '10px',
+  },
+  nestedAccordion: {
+    flexDirection: 'column',
+  },
+});
 
-function App() {
+export default function App() {
+  const classes = useStyles();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(statusData);
+  }, []);
+  const AccordionSummary = withStyles({
+    root: {
+      backgroundColor: 'black',
+    },
+    content: {
+      '&$expanded': {
+        margin: '12px 0',
+      },
+    },
+    expanded: {},
+  })(MuiAccordionSummary);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>BP WAF</h1>
+      <Accordion className={classes.accordion}>
+  <AccordionSummary expandIcon={<StyledExpandMoreIcon />} >
+    <h2>Development</h2>
+  </AccordionSummary>
+  <AccordionDetails>
+    <Accordion>
+      <AccordionSummary expandIcon={<StyledExpandMoreIcon />}>
+        <h3>Blocked IPsets</h3>
+      </AccordionSummary>
+      <AccordionDetails>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="IP Status Table">
+            <TableHead>
+              <TableRow>
+                <TableCell>IP Address</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+                    {statusData.map((row) => (
+                      <TableRow key={row.ip}>
+                        <TableCell component="th" scope="row">
+                          {row.ip}
+                        </TableCell>
+                        <TableCell>{row.status}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+
+          </Table>
+        </TableContainer>
+      </AccordionDetails>
+    </Accordion>
+    <Accordion>
+      <AccordionSummary expandIcon={<StyledExpandMoreIcon />}>
+        <h3>WhiteListed IPsets</h3>
+      </AccordionSummary>
+      <AccordionDetails>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="IP Status Table">
+            <TableHead>
+              <TableRow>
+                <TableCell>IP Address</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {statusData.map((row) => (
+                <TableRow key={row.ip}>
+                  <TableCell component="th" scope="row">
+                    {row.ip}
+                  </TableCell>
+                  <TableCell>{row.status}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </AccordionDetails>
+    </Accordion>
+  </AccordionDetails>
+</Accordion>
+      <Accordion className={classes.accordion}>
+  <AccordionSummary expandIcon={<StyledExpandMoreIcon />}>
+    <h2>Production</h2>
+  </AccordionSummary>
+  <AccordionDetails>
+    <Accordion>
+      <AccordionSummary expandIcon={<StyledExpandMoreIcon />}>
+        <h3>Blocked IPsets</h3>
+      </AccordionSummary>
+      <AccordionDetails>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="IP Status Table">
+            <TableHead>
+              <TableRow>
+                <TableCell>IP Address</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {statusData.map((row) => (
+                <TableRow key={row.ip}>
+                  <TableCell component="th" scope="row">
+                    {row.ip}
+                  </TableCell>
+                  <TableCell>{row.status}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </AccordionDetails>
+    </Accordion>
+    <Accordion>
+      <AccordionSummary expandIcon={<StyledExpandMoreIcon />}>
+        <h3>WhiteListed IPsets</h3>
+      </AccordionSummary>
+      <AccordionDetails>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="IP Status Table">
+            <TableHead>
+              <TableRow>
+                <TableCell>IP Address</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {statusData.map((row) => (
+                <TableRow key={row.ip}>
+                  <TableCell component="th" scope="row">
+                    {row.ip}
+                  </TableCell>
+                  <TableCell>{row.status}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </AccordionDetails>
+    </Accordion>
+  </AccordionDetails>
+</Accordion>
+
+      
     </div>
   );
 }
-
-export default App;
